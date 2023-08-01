e03e83b254f1f0c5dfd9d7aeab0d0c589778a76f
scul0405 <90521402+scul0405@users.noreply.github.com>
2023-08-01 21:34:53 +0700

    Add unit test for auth pg_repo

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/scul0405/blog-clean-architecture-rest-api
 go 1.20
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/go-playground/validator/v10 v10.14.1
 	github.com/google/uuid v1.3.0
 	github.com/jackc/pgx v3.6.2+incompatible
@@ -11,6 +12,7 @@ require (
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/viper v1.16.0
+	github.com/stretchr/testify v1.8.4
 	github.com/uber/jaeger-client-go v2.30.0+incompatible
 	github.com/uber/jaeger-lib v2.4.1+incompatible
 	go.uber.org/zap v1.24.0
@@ -19,6 +21,7 @@ require (
 require (
 	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
 	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -34,6 +37,7 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
---
+++ internal/auth/repository/pg_repo_test.go
@@ -0,0 +1,85 @@
+package repository
+
+import (
+	"context"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	"github.com/scul0405/blog-clean-architecture-rest-api/internal/models"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestAuthRepo_Register(t *testing.T) {
+	t.Parallel()
+
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	defer db.Close()
+
+	sqlxDB := sqlx.NewDb(db, "sqlmock")
+	defer sqlxDB.Close()
+
+	authRepo := NewAuthRepository(sqlxDB)
+
+	t.Run("Register", func(t *testing.T) {
+		gender := "male"
+		role := "user"
+
+		rows := sqlmock.NewRows([]string{"first_name", "last_name", "password", "email", "role", "gender"}).AddRow(
+			"Liem", "Le", "123123", "liemledeptrai@gmail.com", "user", &gender)
+
+		user := &models.User{
+			FirstName: "Liem",
+			LastName:  "Le",
+			Email:     "liemledeptrai@gmail.com",
+			Password:  "123123",
+			Role:      &role,
+			Gender:    &gender,
+		}
+
+		mock.ExpectQuery(createUserQuery).WithArgs(&user.FirstName, &user.LastName, &user.Email,
+			&user.Password, &user.Role, &user.About, &user.Avatar, &user.PhoneNumber, &user.Address, &user.City,
+			&user.Gender, &user.Postcode, &user.Birthday).WillReturnRows(rows)
+
+		createdUser, err := authRepo.Register(context.Background(), user)
+
+		require.NoError(t, err)
+		require.NotNil(t, createdUser)
+		require.Equal(t, createdUser, user)
+	})
+}
+
+func TestAuthRepo_GetByID(t *testing.T) {
+	t.Parallel()
+
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(t, err)
+	defer db.Close()
+
+	sqlxDB := sqlx.NewDb(db, "sqlmock")
+	defer sqlxDB.Close()
+
+	authRepo := NewAuthRepository(sqlxDB)
+
+	t.Run("GetByID", func(t *testing.T) {
+		uID := uuid.New()
+
+		rows := sqlmock.NewRows([]string{"user_id", "first_name", "last_name", "email"}).AddRow(
+			uID, "Liem", "Le", "liemledeptrai@gmail.com")
+
+		testUser := &models.User{
+			UserID:    uID,
+			FirstName: "Liem",
+			LastName:  "Le",
+			Email:     "liemledeptrai@gmail.com",
+		}
+
+		mock.ExpectQuery(getUserQuery).WithArgs(uID).WillReturnRows(rows)
+
+		user, err := authRepo.GetByID(context.Background(), uID)
+
+		require.NoError(t, err)
+		require.Equal(t, testUser, user)
+	})
+}
