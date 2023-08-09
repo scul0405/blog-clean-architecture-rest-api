7491b8d26e560df8f4b51247ab456e1a042456ae
scul0405 <90521402+scul0405@users.noreply.github.com>
2023-08-09 23:37:39 +0700

    Update blog model

--- internal/models/blog.go
+++ internal/models/blog.go
@@ -20,10 +20,23 @@ type Blog struct {
 
 // BlogsList contains list of blogs
 type BlogsList struct {
-	TotalCount int     `json:"total_count"`
-	TotalPages int     `json:"total_pages"`
-	Page       int     `json:"page"`
-	Size       int     `json:"size"`
-	HasMore    bool    `json:"has_more"`
-	Blogs      []*Blog `json:"blogs"`
+	TotalCount int         `json:"total_count"`
+	TotalPages int         `json:"total_pages"`
+	Page       int         `json:"page"`
+	Size       int         `json:"size"`
+	HasMore    bool        `json:"has_more"`
+	Blogs      []*BlogBase `json:"blogs"`
+}
+
+// BlogBase contains data when update and response to client
+type BlogBase struct {
+	BlogID    uuid.UUID `json:"blog_id" db:"blog_id"`
+	AuthorID  uuid.UUID `json:"author_id,omitempty" db:"author_id"`
+	Title     string    `json:"title" db:"title" validate:"omitempty,gte=10"`
+	Content   string    `json:"content" db:"content" validate:"omitempty,gte=20"`
+	ImageURL  *string   `json:"image_url,omitempty" db:"image_url" validate:"omitempty,lte=512,url"`
+	Category  *string   `json:"category,omitempty" db:"category" validate:"omitempty,lte=10"`
+	Author    string    `json:"author" db:"author"`
+	CreatedAt time.Time `json:"created_at,omitempty" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at,omitempty" db:"updated_at"`
 }
