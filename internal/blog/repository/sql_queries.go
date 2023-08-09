c109addf83f7119082deb2462f2389f22e71c2bf
scul0405 <90521402+scul0405@users.noreply.github.com>
2023-08-09 14:39:50 +0700

    Update query name

--- internal/blog/repository/sql_queries.go
+++ internal/blog/repository/sql_queries.go
@@ -1,23 +1,23 @@
 package repository
 
 const (
-	createBlog = `INSERT INTO blogs (author_id, title, content, image_url, category, created_at) 
-					VALUES ($1, $2, $3, NULLIF($4, ''), NULLIF($4, ''), now()) 
+	createBlogQuery = `INSERT INTO blogs (author_id, title, content, image_url, category, created_at) 
+					VALUES ($1, $2, $3, NULLIF($4, ''), NULLIF($5, ''), now()) 
 					RETURNING *`
 
-	getBlogByID = `SELECT n.blog_id,
-						   n.title,
-						   n.content,
-						   n.updated_at,
-						   n.image_url,
-						   n.category,
+	getBlogByIDQuery = `SELECT b.blog_id,
+						   b.title,
+						   b.content,
+						   b.updated_at,
+						   b.image_url,
+						   b.category,
 						   CONCAT(u.first_name, ' ', u.last_name) as author,
 						   u.user_id as author_id
-					FROM news n
-							 LEFT JOIN users u on u.user_id = n.author_id
-					WHERE news_id = $1`
+					FROM blogs b
+							 LEFT JOIN users u on u.user_id = b.author_id
+					WHERE blog_id = $1`
 
-	updateBlog = `UPDATE blogs 
+	updateBlogQuery = `UPDATE blogs 
 					SET title = COALESCE(NULLIF($1, ''), title),
 						content = COALESCE(NULLIF($2, ''), content), 
 					    image_url = COALESCE(NULLIF($3, ''), image_url), 
@@ -26,11 +26,12 @@ const (
 					WHERE blog_id = $5
 					RETURNING *`
 
-	deleteBlog = `DELETE FROM blogs WHERE blog_id = $1`
+	deleteBlogQuery = `DELETE FROM blogs WHERE blog_id = $1`
 
-	getBlogs = `SELECT blog_id, author_id, title, content, image_url, category, updated_at, created_at 
-				FROM blogs 
-				ORDER BY created_at, updated_at OFFSET $1 LIMIT $2`
+	listBlogsQuery = `SELECT b.blog_id, b.title, b.content, b.image_url, b.category, b.updated_at, b.created_at,  CONCAT(u.first_name, ' ', u.last_name) as author, u.user_id as author_id 
+				FROM blogs b
+					LEFT JOIN users u on u.user_id = b.author_id
+				ORDER BY b.created_at, b.updated_at OFFSET $1 LIMIT $2`
 
-	getTotalCount = `SELECT COUNT(blog_id) FROM blogs`
+	getTotalCountQuery = `SELECT COUNT(blog_id) FROM blogs`
 )
