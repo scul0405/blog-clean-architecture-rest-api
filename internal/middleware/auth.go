6bcf1c6c0142aacd3a6b2193a047ca251dcfb476
scul0405 <90521402+scul0405@users.noreply.github.com>
2023-08-09 14:37:51 +0700

    Update middleware context value

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,9 +1,8 @@
 package middleware
 
 import (
+	"context"
 	"github.com/labstack/echo/v4"
-	"github.com/scul0405/blog-clean-architecture-rest-api/config"
-	"github.com/scul0405/blog-clean-architecture-rest-api/internal/auth"
 	httpErrors "github.com/scul0405/blog-clean-architecture-rest-api/pkg/http_errors"
 	"github.com/scul0405/blog-clean-architecture-rest-api/pkg/paseto"
 	"go.uber.org/zap"
@@ -11,31 +10,32 @@ import (
 	"strings"
 )
 
-func (mw *MiddlewareManager) AuthPASETOMiddleware(authUC auth.UseCase, cfg *config.Config) echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			bearerHeader := c.Request().Header.Get("Authorization")
-			if bearerHeader == "" {
-				mw.logger.Error("auth middleware", zap.String("bearerHeader", "bearerHeader = \"\""))
-				return c.JSON(http.StatusUnauthorized, httpErrors.NewUnauthorizedError(httpErrors.Unauthorized))
-			}
-
-			headerParts := strings.Split(bearerHeader, " ")
-			if len(headerParts) != 2 {
-				mw.logger.Error("auth middleware", zap.String("headerParts", "len(headerParts) != 2"))
-				return c.JSON(http.StatusUnauthorized, httpErrors.NewUnauthorizedError(httpErrors.Unauthorized))
-			}
-
-			tokenString := headerParts[1]
-			payload, err := paseto.VerifyPASETOToken(tokenString, cfg)
-			if err != nil {
-				mw.logger.Error("auth middleware", zap.String("verifyPASETO", err.Error()))
-				return c.JSON(http.StatusUnauthorized, httpErrors.NewUnauthorizedError(httpErrors.Unauthorized))
-			}
-
-			c.Set("user_id", payload.ID)
-
-			return next(c)
+func (mw *MiddlewareManager) AuthPASETOMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		bearerHeader := c.Request().Header.Get("Authorization")
+		if bearerHeader == "" {
+			mw.logger.Error("auth middleware", zap.String("bearerHeader", "bearerHeader = \"\""))
+			return c.JSON(http.StatusUnauthorized, httpErrors.NewUnauthorizedError(httpErrors.Unauthorized))
 		}
+
+		headerParts := strings.Split(bearerHeader, " ")
+		if len(headerParts) != 2 {
+			mw.logger.Error("auth middleware", zap.String("headerParts", "len(headerParts) != 2"))
+			return c.JSON(http.StatusUnauthorized, httpErrors.NewUnauthorizedError(httpErrors.Unauthorized))
+		}
+
+		tokenString := headerParts[1]
+		payload, err := paseto.VerifyPASETOToken(tokenString, mw.cfg)
+		if err != nil {
+			mw.logger.Error("auth middleware", zap.String("verifyPASETO", err.Error()))
+			return c.JSON(http.StatusUnauthorized, httpErrors.NewUnauthorizedError(httpErrors.Unauthorized))
+		}
+
+		c.Set("user_id", payload.ID)
+
+		ctx := context.WithValue(c.Request().Context(), "user_id", payload.ID)
+		c.SetRequest(c.Request().WithContext(ctx))
+
+		return next(c)
 	}
 }
