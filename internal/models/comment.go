23ad9b8fa1f8c7788f0228f841aeab343280af9c
scul0405 <90521402+scul0405@users.noreply.github.com>
2023-08-12 23:19:45 +0700

    Update comment model

--- internal/models/comment.go
+++ internal/models/comment.go
@@ -9,7 +9,7 @@ import (
 // Comment model
 type Comment struct {
 	CommentID uuid.UUID `json:"comment_id" db:"comment_id" validate:"omitempty,uuid"`
-	AuthorID  uuid.UUID `json:"author_id" db:"author_id" validate:"required"`
+	AuthorID  uuid.UUID `json:"author_id" db:"author_id"`
 	BlogID    uuid.UUID `json:"blog_id" db:"blog_id" validate:"required"`
 	Message   string    `json:"message" db:"message" validate:"required,gte=10"`
 	Likes     int64     `json:"likes" db:"likes" validate:"omitempty"`
@@ -20,11 +20,13 @@ type Comment struct {
 // Base Comment response
 type CommentBase struct {
 	CommentID uuid.UUID `json:"comment_id" db:"comment_id" validate:"omitempty,uuid"`
-	AuthorID  uuid.UUID `json:"author_id" db:"author_id" validate:"required"`
-	Author    string    `json:"author" db:"author" validate:"required"`
+	AuthorID  uuid.UUID `json:"author_id" db:"author_id"`
+	Author    string    `json:"author" db:"author"`
+	BlogID    uuid.UUID `json:"blog_id" db:"blog_id"`
 	AvatarURL *string   `json:"avatar_url" db:"avatar_url"`
 	Message   string    `json:"message" db:"message" validate:"required,gte=10"`
 	Likes     int64     `json:"likes" db:"likes" validate:"omitempty"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
 
